0a60a369036bc96893a0b2f617f36315d47b50f9
EddBr <35238776+EddBr@users.noreply.github.com>
2018-08-11 22:19:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('message', message => {
-  if (message.content.startsWith('ping!')) {
-    message.reply('pong!');
-  }
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
-client.on('ready', () => {
-   console.log('I am ready!'); 
+client.on("message", (message) => {
+    if (message.content){
+    message.channel.send(message);
+    }
 });
 
-client.login(BOT_TOKEN);
-
+client.login(process.env.BOT_TOKEN);
